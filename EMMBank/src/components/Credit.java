9d3335b08f757b34912b129b3e62769a77e0b8c5
XilightLPB <erwan.maigne-montamat@epita.fr>
2023-11-15 09:35:01 +0100

    1.3.3 Creation of the Transfert, Credit, Debit Classes

---
+++ EMMBank/src/components/Credit.java
@@ -0,0 +1,11 @@
+//1.3.3 Creation of the Transfert, Credit, Debit Classes
+package components;
+
+public class Credit extends Flow {
+
+	protected Credit(String comment, double d, int targetAccountNumber) {
+		super(comment, d, targetAccountNumber);
+		
+	}
+
+}
---
+++ EMMBank/src/components/Debit.java
@@ -0,0 +1,11 @@
+//1.3.3 Creation of the Transfert, Credit, Debit Classes
+package components;
+
+public class Debit extends Flow {
+
+	protected Debit(String comment, double d, int targetAccountNumber) {
+		super(comment, d, targetAccountNumber);
+		
+	}
+
+}
